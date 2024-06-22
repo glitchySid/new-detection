cbe31da0eddca2756685d8ef5b5e2c95165a45dc
Sid <157303637+glitchySid@users.noreply.github.com>
2024-06-22 18:35:30 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,40 +1,43 @@
 from flask import Flask, request, render_template 
-import tensorflow as tf
-import json
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import numpy as np
+#import tensorflow as tf
+# import json
+# from tensorflow.keras.preprocessing.text import tokenizer_from_json
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# import numpy as np
 
 app = Flask(__name__, static_url_path='/static') 
 
 # Loading Model and Tokenizer
-with open('tokenizer.json', 'r') as t:
-    data = json.load(t)
-    tokenizer = tokenizer_from_json(data)
-model = tf.keras.models.load_model('newsdetection.keras')
+# with open('tokenizer.json', 'r') as t:
+#     data = json.load(t)
+#     tokenizer = tokenizer_from_json(data)
+# model = tf.keras.models.load_model('newsdetection.keras')
 
 
-@app.route('/', methods=['GET', 'POST']) 
-def index(): 
-    if request.method == 'POST': 
-        # Retrieve the text from the textarea 
-        text = request.form.get('textarea') 
-        print("length",len(text))
-        if len(text)==0:
-            return render_template('index.html', content="Please Provide News") 
-        else:
-            new_sequence = tokenizer.texts_to_sequences([text])
-            padded_new_sequence = pad_sequences(new_sequence, maxlen=50)
-            prediction = model.predict(padded_new_sequence)
-            if np.argmax(prediction)==1:
-                print("News is Real")
-                predicted_text = "News is Real"
-            else:
-                print("News is Fake")
-                predicted_text = "News is Fake"
-            # Print the text in terminal for verification 
-            print(text) 
-            return render_template('index.html', content=predicted_text) 
+# @app.route('/', methods=['GET', 'POST']) 
+# def index(): 
+#     if request.method == 'POST': 
+#         # Retrieve the text from the textarea 
+#         text = request.form.get('textarea') 
+#         print("length",len(text))
+#         if len(text)==0:
+#             return render_template('index.html', content="Please Provide News") 
+#         else:
+#             new_sequence = tokenizer.texts_to_sequences([text])
+#             padded_new_sequence = pad_sequences(new_sequence, maxlen=50)
+#             prediction = model.predict(padded_new_sequence)
+#             if np.argmax(prediction)==1:
+#                 print("News is Real")
+#                 predicted_text = "News is Real"
+#             else:
+#                 print("News is Fake")
+#                 predicted_text = "News is Fake"
+#             # Print the text in terminal for verification 
+#             print(text) 
+#             return render_template('index.html', content=predicted_text) 
+#     return render_template('index.html')
+
+def index():
     return render_template('index.html')
   
 if __name__ == "__main__":
